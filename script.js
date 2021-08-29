8339f95efe8df13f366fccee12ea0aa3c0b14e75
Vasil <bobrovskiy.edu@gmail.com>
2021-08-29 19:07:49 +0300

    v1.0

---
+++ script.js
@@ -0,0 +1,21 @@
+
+'use strict';
+const number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Ваш последний просмотренный фильм?', ''),
+      b = prompt('На сколько вы его оцените?', ''),
+      c = prompt('Ваш последний просмотренный фильм?', ''),
+      d = prompt('На сколько вы его оцените?', '');
+       
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
