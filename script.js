38c08450ce9812d202b05fdf50ab3454139bafdf
Ilya <bobrovskiy.edu@gmail.com>
2021-09-09 18:09:46 +0300

    Practice_3

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 
 'use strict';
-const number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let number0fFilms;
+
+function start() {
+    number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while (number0fFilms == '' || number0fFilms == null || isNaN(number0fFilms)) {
+        number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+start();
 
 const personalMovieDB = {
     count: number0fFilms,
@@ -9,16 +17,23 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-for (let i = 0; i < 2; i++){
-    const a = prompt('Ваш последний просмотренный фильм?', ''),
-          b = prompt('На сколько вы его оцените?', '');
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        console.log('nice');
-    } else {
-        console.log('error');
-        i--;
-    }
 
+function rememberMyFilms(){
+    for (let i = 0; i < 2; i++){
+        const a = prompt('Ваш последний просмотренный фильм?', ''),
+              b = prompt('На сколько вы его оцените?', '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            console.log('nice');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+rememberMyFilms();
+
+
+function detectPersonalLevel(){
     if (personalMovieDB.count < 10) {
         console.log("Маловато, вы точно не киноман:р");
     } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
@@ -29,8 +44,10 @@ for (let i = 0; i < 2; i++){
         console.log('error');
     }
 
-personalMovieDB.movies[a] = b;
 }
+    detectPersonalLevel();
+//personalMovieDB.movies[a] = b;
+
 
 
 // if (a == 10) {
@@ -42,13 +59,25 @@ personalMovieDB.movies[a] = b;
 // (a == 10) ? alert('вЫ ПИдор') : alert('Нахуй идите');
     
 
-console.log(personalMovieDB);
+// let f = "something";
+// console.log(f.length);
 
-let f = "something";
-console.log(f.length);
+// function shfs(text) {
+//     console.log(text);
+// }
 
-function shfs(text) {
-    console.log(text);
+// shfs("pisun");
+function writeYourGenres(){
+    for(let i = 1; i <= 3; i++){
+        let a = prompt(`Ваш любимый жанр под номером ${i}?`);
+        
+        personalMovieDB.genres[i - 1] = a;
+       }
 }
-
-shfs("pisun");
+function privatDB(){
+    if (personalMovieDB.privat != false){
+        console.log(personalMovieDB);
+    }
+}
+writeYourGenres();
+privatDB();
